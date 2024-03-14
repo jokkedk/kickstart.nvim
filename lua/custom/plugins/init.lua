c21bffaa979bc643d887c1f26f3f963537c0edaf
Joakim <joakim@scienta.dk>
2024-03-14 13:30:39 +0100

    Add: custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,28 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    'ThePrimeagen/vim-be-good',
+    -- {
+    --     'akinsho/bufferline.nvim',
+    --     version = '*',
+    --     -- dependencies = 'nvim-tree/nvim-web-devicons',
+    --     opts = {},
+    -- },
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("refactoring").setup()
+        end,
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    'nvim-treesitter/nvim-treesitter-context',
+}
